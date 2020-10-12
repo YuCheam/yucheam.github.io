bdbf7f43b62b06b5de969ef34a1a1fdd8ec05c37
Yucheam <k.yucheam@yahoo.com>
2020-10-12 01:17:39 -0400

    Fixing page and index transitions

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,23 @@
 <template>
   <div>
     <div id="typewriter">Hello my name is Kha-Yu Aimee Cheam</div>
+    
+    <div id="appear">
+        <p>Senior @ PSU | Blue Band Trumpet | Hopeful Developer</p>
+        <p>I like to code cools things, and learn many stuffs</p>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+})
 </script>
 
 <style>
-#typewriter {
-  font-size: 4vw;
-  overflow: hidden;
-  border-right: .1em solid orange;
-  white-space: nowrap;
-  margin: 0 auto;
-  letter-spacing: .1em;
-  animation:
-    typing 4s steps(45,end),
-    blink-caret .75s step-end 0s 10 forwards;
-}
-
 /* Type writer effect */
 @keyframes typing {
   from {width: 0}
@@ -32,5 +27,29 @@ export default Vue.extend({})
 @keyframes blink-caret {
   from, to {border-color: transparent}
   50% {border-color: #ff9a8d;}
+}
+
+@keyframes timedAppearance {
+  from { opacity: 0}
+  to { opacity: 1}
+}
+
+#typewriter {
+  font-size: 4vw;
+  overflow: hidden;
+  border-right: .1em solid orange;
+  white-space: nowrap;
+  margin: 0 auto;
+  letter-spacing: .1em;
+  animation:
+    typing 3s steps(45,end),
+    blink-caret .75s step-end 0s 10 forwards;
+}
+
+#appear p {
+  animation-name: timedAppearance;
+  animation-duration: 1s;
+  animation-delay: 3s;
+  animation-fill-mode: backwards;
 }
 </style>
